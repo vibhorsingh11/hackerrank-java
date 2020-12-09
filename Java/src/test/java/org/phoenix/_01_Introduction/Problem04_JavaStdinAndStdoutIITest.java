ac64f2909f304cf6e76196a0519e51703fa4e70b
vibhorsingh11 <vibhorsinghcs@gmail.com>
2020-12-09 19:04:19 +0530

    Added Problem 04 Java Stdin And Stdout II

---
+++ Java/src/test/java/org/phoenix/_01_Introduction/Problem04_JavaStdinAndStdoutIITest.java
@@ -0,0 +1,12 @@
+package org.phoenix._01_Introduction;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Problem04_JavaStdinAndStdoutIITest {
+
+    @Test
+    void main() {
+    }
+}
