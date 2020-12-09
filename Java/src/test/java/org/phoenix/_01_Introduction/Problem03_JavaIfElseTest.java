6426c908b070c99d9ff50abb522265e4c86139e0
vibhorsingh11 <vibhorsinghcs@gmail.com>
2020-12-09 18:32:36 +0530

    Added Problem 03 Java If Else Test

---
+++ Java/src/test/java/org/phoenix/_01_Introduction/Problem03_JavaIfElseTest.java
@@ -0,0 +1,12 @@
+package org.phoenix._01_Introduction;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Problem03_JavaIfElseTest {
+
+    @Test
+    void main() {
+    }
+}
