382c90a85c351af5cf2312f3738df31b6e93acce
vibhorsingh11 <vibhorsinghcs@gmail.com>
2020-12-17 23:13:27 +0530

    Added Problem 01 Varargs Simple Addition

---
+++ Java/src/main/java/org/phoenix/_07_Advanced/Problem01_VarargsSimpleAddition.java
@@ -0,0 +1,59 @@
+package org.phoenix._07_Advanced;
+
+/*
+ * The task is to create the class Add and the required methods so that the code prints the sum of the numbers
+ * passed to the function add.*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Problem01_VarargsSimpleAddition {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int n1 = Integer.parseInt(br.readLine());
+            int n2 = Integer.parseInt(br.readLine());
+            int n3 = Integer.parseInt(br.readLine());
+            int n4 = Integer.parseInt(br.readLine());
+            int n5 = Integer.parseInt(br.readLine());
+            int n6 = Integer.parseInt(br.readLine());
+            Add ob = new Add();
+            ob.add(n1, n2);
+            ob.add(n1, n2, n3);
+            ob.add(n1, n2, n3, n4, n5);
+            ob.add(n1, n2, n3, n4, n5, n6);
+            Method[] methods = Add.class.getDeclaredMethods();
+            Set<String> set = new HashSet<>();
+            boolean overload = false;
+            for (Method method : methods) {
+                if (set.contains(method.getName())) {
+                    overload = true;
+                    break;
+                }
+                set.add(method.getName());
+
+            }
+            if (overload) {
+                throw new Exception("Overloading not allowed");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Add {
+    void add(int... args) {
+        String s = "";
+        int sum = 0;
+        for (int i : args) {
+            sum += i;
+            System.out.print(s + i);
+            s = "+";
+        }
+        System.out.println("=" + sum);
+    }
+}
